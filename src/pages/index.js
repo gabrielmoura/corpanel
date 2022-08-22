ae2b64519e17ba1fce975294801decc5090cbda2
Gabriel Moura <gabriel.blx32@gmail.com>
2022-08-22 18:04:33 -0300

    Reordena exibição.

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,35 +26,38 @@ export default function Home() {
             <Nav2/>
             <div className="container">
                 <TopPanel refetch={refetch}/>
+                <div className="row">
+                    <div className="col-6">
+                        <div className="my-3 p-3 bg-body rounded shadow-sm">
+                            <h6 className="border-bottom pb-2 mb-0">Abertos {(isLoading) ? (
+                                <p>Carregando</p>) : data?.open.eventos.length}</h6>
 
-                <div className="my-3 p-3 bg-body rounded shadow-sm">
-                    <h6 className="border-bottom pb-2 mb-0">Abertos {(isLoading) ? (
-                        <p>Carregando</p>) : data?.open.eventos.length}</h6>
+                            {(isLoading) ? (<p>Carregando</p>) : data?.open.eventos.map(evento => {
+                                if (evento.status == 'ABERTO') {
+                                    return <EventoCor evento={evento}></EventoCor>
+                                }
+                            })}
+                            <small className="d-block text-end mt-3">
+                                {/*<a href="#">All updates</a>*/}
+                            </small>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <div className="my-3 p-3 bg-body rounded shadow-sm">
+                            <h6 className="border-bottom pb-2 mb-0">Fechados {(isLoading) ? (
+                                <p>Carregando</p>) : data?.close.eventos.length}</h6>
+                            {(isLoading) ? (<p>Carregando</p>) : data?.close.eventos.map(evento => {
+                                if (evento.status == 'FECHADO') {
+                                    return <EventoCor evento={evento}></EventoCor>
+                                }
+                            })}
 
-                    {(isLoading) ? (<p>Carregando</p>) : data?.open.eventos.map(evento => {
-                        if (evento.status == 'ABERTO') {
-                            return <EventoCor evento={evento}></EventoCor>
-                        }
-                    })}
-                    <small className="d-block text-end mt-3">
-                        {/*<a href="#">All updates</a>*/}
-                    </small>
+                            <small className="d-block text-end mt-3">
+                                {/*<a href="#">All suggestions</a>*/}
+                            </small>
+                        </div>
+                    </div>
                 </div>
-
-                <div className="my-3 p-3 bg-body rounded shadow-sm">
-                    <h6 className="border-bottom pb-2 mb-0">Fechados {(isLoading) ? (
-                        <p>Carregando</p>) : data?.close.eventos.length}</h6>
-                    {(isLoading) ? (<p>Carregando</p>) : data?.close.eventos.map(evento => {
-                        if (evento.status == 'FECHADO') {
-                            return <EventoCor evento={evento}></EventoCor>
-                        }
-                    })}
-
-                    <small className="d-block text-end mt-3">
-                        {/*<a href="#">All suggestions</a>*/}
-                    </small>
-                </div>
-
             </div>
         </>
     )
